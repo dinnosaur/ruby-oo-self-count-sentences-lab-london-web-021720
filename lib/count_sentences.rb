0fe9be3a9484675f3f94718e4a0f6bebb7a829bc
dinnosaur <dinnoroni@gmail.com>
2020-02-20 16:16:25 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+   array = self.split(/[.!?]/)
+   new_array = []
+   array.each { |space| 
+  if space != ""
+    new_array << space
+  end 
+  }
 
+
+   
+  new_array.length 
+   
+ 
+
+
+
+ 
   end
 end
+
